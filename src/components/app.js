3c912fe7756256ae9bae6b029bd893ff5975c8c2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-10-07 21:42:35 -0400

    wire up axios

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentCampers: []
+    };
+
+    this.fetchRecentCampers();
+  }
+
+  fetchRecentCampers() {
+    console.log('before:', this.state.recentCampers);
+    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then((response) => {
+        this.setState({recentCampers: response.data});
+        console.log('after:', this.state.recentCampers);
+      });
+  }
+
   render() {
+    if (!this.state.recentCampers.length) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>React simple starter</div>
+      <div>Finished Loading!</div>
     );
   }
 }
